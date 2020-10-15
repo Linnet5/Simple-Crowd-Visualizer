1f0252aecb6073a790586e04d03862cbfb5b6515
Linus <itslinnet@gmail.com>
2020-10-15 11:31:17 +0200

    Updates

---
+++ precache-manifest.2d007ff63db583cc858125fbb2dc334b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622c1fb3e322b0e541bd3c42d60c4fad",
+    "url": "/simple-crowd-visualizer/index.html"
+  },
+  {
+    "revision": "f2f5d36e40fb3dbf75ff",
+    "url": "/simple-crowd-visualizer/static/css/main.719bfb28.chunk.css"
+  },
+  {
+    "revision": "16ba04845e2e3bad33b2",
+    "url": "/simple-crowd-visualizer/static/js/2.1dca66e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-crowd-visualizer/static/js/2.1dca66e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f5d36e40fb3dbf75ff",
+    "url": "/simple-crowd-visualizer/static/js/main.b1a93d73.chunk.js"
+  },
+  {
+    "revision": "b2fde098d48ac4c2bf5b",
+    "url": "/simple-crowd-visualizer/static/js/runtime-main.49e0aaa1.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/simple-crowd-visualizer/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "897cdf32c3e9e684dc9bd46073b30cd8",
+    "url": "/simple-crowd-visualizer/static/media/person-blue.897cdf32.svg"
+  },
+  {
+    "revision": "83c15df9f24d972d7a726b0d0e339348",
+    "url": "/simple-crowd-visualizer/static/media/person-red.83c15df9.svg"
+  }
+]);
